eea3c326e0c021fd7af20311dc867a97df3ec372
Zarif Karim <zkarim@atlassian.com>
2023-09-16 05:28:41 +1000

    challenge2 done

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function Board({ rows, cols, xturn, squares, onPlay }) {
 /*
   Challenges:
   1. [Done] For the current move only, show “You are at move #…” instead of a button.
-  2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
+  2. [Done] Rewrite Board to use two loops to make the squares instead of hardcoding them.
   3. Add a toggle button that lets you sort the moves in either ascending or descending order.
   4. When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
   5. Display the location for each move in the format (row, col) in the move history list.
